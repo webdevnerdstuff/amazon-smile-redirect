d9da90521634fb06b8f0d4438bcef1cc2fbd044c
webdevnerdstuff <webdevnerdstuff@gmail.com>
2020-02-08 19:23:02 -0800

    Refactoring and fixing bug

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,105 +1,72 @@
 let extensionStatus;
-let goToLogin;
-let tabs = [];
 
 // ---------------------------------------------------- Runtime //
 // -------------------------- Get Set Storage //
-chrome.storage.local.get(['extensionStatus', 'goToLogin', 'tabs'], result => {
-	extensionStatus = result.extensionStatus || 'enabled';
-	goToLogin = result.goToLogin || false;
-	tabs = result.tabs || [];
+chrome.storage.local.get(['extensionStatus'], result => {
+  extensionStatus = result.extensionStatus || 'enabled';
 
-	chrome.storage.local.set({ extensionStatus, goToLogin, tabs }, () => {});
+  chrome.storage.local.set({ extensionStatus }, () => { });
 
-	updateIcon();
+  updateIcon();
 });
 
 // -------------------------- On Message //
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	// Check Page Load Count //
-	if (request.checkPageLoadCount) {
-		// If the extension is disabled //
-		if (extensionStatus === 'disabled') {
-			sendResponse({ extensionStatus, pageLoadCount: 0 });
-			tabs = [];
-			return false;
-		}
+  // Checking if user is logged out //
+  if (request.logOutCheck) {
+    const goToLogin = request.loggedOut;
 
-		const pageLoadCount = checkPageLoadCount(sender.tab);
-		goToLogin = pageLoadCount > 1 || false;
-		chrome.storage.local.set({ goToLogin }, () => {});
+    sendResponse({ extensionStatus, goToLogin });
+    return false;
+  }
 
-		// Reset tabs //
-		if (pageLoadCount > 1) {
-			delete tabs[sender.tab.id];
-		}
+  // Get Extension Status //
+  if (request.getExtensionStatus) {
+    sendResponse({ extensionStatus });
+    return false;
+  }
 
-		sendResponse({ extensionStatus, goToLogin });
-		return false;
-	}
+  // Toggle Status //
+  if (request.toggleStatus) {
+    const status = updateStatus(extensionStatus);
+    sendResponse({ extensionStatus: status });
+  }
 
-	// Get Extension Status //
-	if (request.getExtensionStatus) {
-		sendResponse({ extensionStatus });
-		return false;
-	}
-
-	// Toggle Status //
-	if (request.toggleStatus) {
-		const status = updateStatus(extensionStatus);
-		sendResponse({ extensionStatus: status });
-	}
-
-	return false;
+  return false;
 });
 
 // ---------------------------------------------------- Update Popup Status Icon //
 function updateIcon() {
-	if (extensionStatus === 'disabled') {
-		chrome.browserAction.setIcon({
-			path: {
-				16: 'assets/images/icon-disabled16.png',
-				48: 'assets/images/icon-disabled48.png',
-				128: 'assets/images/icon-disabled128.png',
-			},
-		});
+  if (extensionStatus === 'disabled') {
+    chrome.browserAction.setIcon({
+      path: {
+        16: 'assets/images/icon-disabled16.png',
+        48: 'assets/images/icon-disabled48.png',
+        128: 'assets/images/icon-disabled128.png',
+      },
+    });
 
-		return false;
-	}
+    return false;
+  }
 
-	chrome.browserAction.setIcon({
-		path: {
-			16: 'assets/images/icon16.png',
-			48: 'assets/images/icon48.png',
-			128: 'assets/images/icon128.png',
-		},
-	});
+  chrome.browserAction.setIcon({
+    path: {
+      16: 'assets/images/icon16.png',
+      48: 'assets/images/icon48.png',
+      128: 'assets/images/icon128.png',
+    },
+  });
 
-	return false;
+  return false;
 }
 
 // ---------------------------------------------------- Update Status //
 function updateStatus() {
-	extensionStatus = extensionStatus === 'enabled' ? 'disabled' : 'enabled';
+  extensionStatus = extensionStatus === 'enabled' ? 'disabled' : 'enabled';
 
-	chrome.storage.local.set({ extensionStatus }, () => {});
+  chrome.storage.local.set({ extensionStatus }, () => { });
 
-	updateIcon();
+  updateIcon();
 
-	return extensionStatus;
-}
-
-// ---------------------------------------------------- Check Page Load Count //
-function checkPageLoadCount(tab) {
-	const tabId = tab.id;
-
-	if (!tabs[tabId]) {
-		tabs[tabId] = 1;
-	}
-	else {
-		tabs[tabId] += 1;
-	}
-
-	chrome.storage.local.set({ tabs }, () => {});
-	return tabs[tabId];
+  return extensionStatus;
 }
