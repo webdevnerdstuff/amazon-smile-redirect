7441716955a9e29c0f6ebc4d6cc5a010f91e98b5
webdevnerdstuff <webdevnerdstuff@gmail.com>
2020-06-15 08:37:49 -0700

    Changing innerhtml to innertext

--- assets/js/about.js
+++ assets/js/about.js
@@ -2,5 +2,5 @@ const manifest = chrome.runtime.getManifest();
 const date = new Date();
 const year = date.getFullYear();
 
-document.getElementById('version-number').innerHTML = manifest.version;
-document.getElementById('copyright-year').innerHTML = year;
+document.getElementById('version-number').innerText = manifest.version;
+document.getElementById('copyright-year').innerText = year;
